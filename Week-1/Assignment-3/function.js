368e4bff64b941a9b682d04da34bde4d17cdb7eb
Hao Yu Wang <awaitcabal@hotmail.com>
2022-03-26 21:26:58 +0800

    assignment-3

---
+++ Week-1/Assignment-3/function.js
@@ -0,0 +1,22 @@
+function countAandB(input) {
+    var count = 0
+    for (var i = 0; i < input.length; i++) {
+        if (input[i] === "a" || input[i] === "b") {
+            count += 1;
+        }
+    }
+    return count;
+}
+function toNumber(input) {
+    var out = []
+    for (var i = 0; i < input.length; i++) {
+        out.push(input[i].charCodeAt(0) - 96)
+    }
+    return out;
+}
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1, 3]
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
