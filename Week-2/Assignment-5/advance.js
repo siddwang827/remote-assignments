34eca5a3ef535e49fa2dd1e32269647e1c44311c
Hao Yu Wang <awaitcabal@hotmail.com>
2022-04-02 22:36:41 +0800

    advance add target not in array

--- Week-2/Assignment-5/advance.js
+++ Week-2/Assignment-5/advance.js
@@ -1,22 +1,29 @@
 function binarySearchPosition(numbers, target) {
     let left = 0;
     let right = numbers.length - 1;
+    let index = -1;
+    let mid;
 
     do {
-        var mid = Math.floor((left + right) / 2);
+        mid = Math.floor((left + right) / 2);
 
         if (target === numbers[mid]) {
-            return mid;
+            index = mid
+            return index;
+
         } else if (target < numbers[mid]) {
-            right = mid;
+            right = mid - 1;
+
         } else {
             left = mid + 1;
         }
 
-    } while (numbers[mid] !== target)
+    } while (left <= right)
+
+    return index
 
 
 
 }
-console.log(binarySearchPosition([1, 3, 4, 6, 8, 11, 12], 11));
+console.log(binarySearchPosition([1, 3, 4, 6, 8, 11, 12], 10));
 console.log(binarySearchPosition([1, 2, 5, 6, 7, 10], 7));
