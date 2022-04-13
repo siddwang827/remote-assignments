9b21885c3621e670f29444bd22bc576d12fcaf33
Hao Yu Wang <awaitcabal@hotmail.com>
2022-04-14 00:29:08 +0800

    assignment2 complete

--- Week-4/Assignment-1/app.js
+++ Week-4/Assignment-1/app.js
@@ -1,15 +1,13 @@
 
 function delayedResult(n1, n2, delayTime, callback) {
-    setTimeout(() => {
-        const addResult = n1 + n2;
-        callback(addResult)
-    }, delayTime)
+    setTimeout(() => callback(n1 + n2), delayTime)
 }
 
 
 delayedResult(4, 5, 3000, function (result) {
     console.log(result);
 }); // 9 (4+5) will be shown in the console after 3 seconds
+
 delayedResult(-5, 10, 2000, function (result) {
     console.log(result);
 }); // 5 (-5+10) will be shown in the console after 2 seconds
---
+++ Week-4/Assignment-2/ajaxRequest.js
@@ -0,0 +1,33 @@
+const btnAjax = document.getElementById('btn-ajax')
+
+function ajax(src, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200)
+            callback(JSON.parse(this.responseText));
+    };
+    xhr.open('GET', src);
+    xhr.send();
+}
+
+function render(data) {
+    const resultAjax = document.getElementById('result-ajax');
+
+    for (dataItem of data) {
+        const dataHTML = document.createElement('div')
+        const body = ` 
+            <h2>${dataItem.name}&emsp;<font size="2">售價:&nbsp; $${dataItem.price}</font></h2>
+            <p>- <em>${dataItem.description}</em></p>
+        `
+        dataHTML.innerHTML = body
+        resultAjax.appendChild(dataHTML)
+    }
+}
+
+btnAjax.addEventListener('click', () => {
+    const urlAjax = document.getElementById('url-ajax');
+    ajax(urlAjax.value, response => render(response));
+
+
+
+});
