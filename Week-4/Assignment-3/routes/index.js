f8b56cc7a07ad45f21bd0feff66a9dcc28377f69
Hao Yu Wang <awaitcabal@hotmail.com>
2022-04-16 13:35:55 +0800

    function correct

--- Week-4/Assignment-3/routes/index.js
+++ Week-4/Assignment-3/routes/index.js
@@ -1,70 +1,58 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt'); 
 const db = require("../database");
-const { render } = require('express/lib/response');
 const router = express.Router();
 
 
-function queryUserInfo() {
-
-}
-
 function checkDuplicated(req, res, next) {
-    console.log("in checkDuplicated function")
-    const userEmail = req.body.email;
+    const checkEmail = req.body.email;
     let sql = "SELECT email FROM assignment.user Where email = ?";
-    db.query(sql, [userEmail], (err, result) => {
+    db.query(sql, [checkEmail], (err, result) => {
         if (err) throw err;
         if (result[0]) {
             const errorMessage = 'This email has already been used!'
-            console.log(errorMessage);
             res.render('user', { errorMessage })
         } else {
-            console.log('This email is unique.');
             next()
         }
     });
-
 };
 
-
-
-
-function checkMatch(userInfo) {
-    let sql = "SELECT * FROM user "
+function checkValid(req, res, next) {
+    const checkInfo = req.body;
+    let sql = "SELECT email, password FROM assignment.user WHERE email = ? "
+    db.query(sql, [checkInfo.email, checkInfo.password], (err, result) => {
+        if (err) throw err;
+        if (!result) {
+            const errorMessage = 'This email is not exist!'
+            res.render('user', { errorMessage })
+        } else {
+            if (checkInfo.password !== result[0].password) {
+                const errorMessage = 'Wrong Password!'
+                res.render('user', { errorMessage })
+            }
+            else {
+                console.log('Login Valid')
+                next()
+            }
+        }
+    })
 };
-
-
-function logIn(userInfo) {
-
-};
-
 
 function register(req, res, next) {
-    let { email, password } = req.body;
+    const { email, password } = req.body;
     let sql = "INSERT INTO assignment.user (email, password) VALUES (?, ?)";
     db.query(sql, [email, password], (err, result) => {
         if (err) throw err;
-        console.log(result)
         next()
     });
-
-
 };
-
-
-
-
-
-
-
-
 
 
 
 
 router.get("/", (req, res) => {
-    console.log("root!")
+    res.redirect('/home')
 })
 
 router.get("/home", (req, res) => {
@@ -78,18 +66,15 @@ router.get("/home", (req, res) => {
 })
 
 router.post("/signup", [checkDuplicated, register], (req, res) => {
-    console.log("new email sign up success")
     res.cookie('email', req.body.email)
     res.redirect('/member')
 
 })
 
 
-router.post("/login", [], (req, res) => {
-    const { email, password } = req.body
-    console.log(req.body)
+router.post("/login", checkValid, (req, res) => {
+    res.cookie('email', req.body.email)
     res.redirect('/member')
-
 
 })
 
