d81e046095504e7f0100e61568be31cddd3626c4
Hao Yu Wang <awaitcabal@hotmail.com>
2022-04-19 01:07:23 +0800

    promise async

--- Week-4/Assignment-2/ajaxRequest.js
+++ Week-4/Assignment-2/ajaxRequest.js
@@ -1,33 +1,62 @@
-const btnAjax = document.getElementById('btn-ajax')
+const url = 'https://appworks-school.github.io/Remote-Aassigiment-Data/products'
+const btnCallback = document.getElementById('btn-callback')
+const btnPromise = document.getElementById('btn-promise')
+const result = document.querySelector('.result ');
 
-function ajax(src, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200)
-            callback(JSON.parse(this.responseText));
-    };
-    xhr.open('GET', src);
-    xhr.send();
+// ajax callback
+function ajaxCallback(src, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', src);
+	xhr.onload = () => {
+		if (xhr.status === 200)
+			callback(JSON.parse(xhr.responseText));
+	};
+	xhr.send();
 }
+
+// promise
+function ajaxPromise(src) {
+	return new Promise((resolve, reject) => {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', src);
+		xhr.onload = () => {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				resolve(JSON.parse(xhr.responseText));
+			} else {
+				reject(Error(xhr.statusText))
+			}
+		};
+		xhr.onerror = () => reject(Error('network error'))
+		xhr.send();
+	})
+}
+
+// async & await
+
+
 
 function render(data) {
-    const resultAjax = document.getElementById('result-ajax');
 
-    for (dataItem of data) {
-        const dataHTML = document.createElement('div')
-        const body = ` 
-            <h2>${dataItem.name}&emsp;<font size="2">售價:&nbsp; $${dataItem.price}</font></h2>
-            <p>- <em>${dataItem.description}</em></p>
+	data.forEach(item => {
+		const dataHTML = document.createElement('div')
+		const body = ` 
+            <h2>${item.name}&emsp;<font size="2">售價:&nbsp; $${item.price}</font></h2>
+            <p>- <em>${item.description}</em></p>
         `
-        dataHTML.innerHTML = body
-        resultAjax.appendChild(dataHTML)
-    }
+		dataHTML.innerHTML = body
+		result.appendChild(dataHTML)
+	})
 }
 
-btnAjax.addEventListener('click', () => {
-    const urlAjax = document.getElementById('url-ajax');
-    ajax(urlAjax.value, render);
+btnCallback.addEventListener('click', () => {
+	ajaxCallback(url, render);
+});
 
-
-
+btnPromise.addEventListener('click', () => {
+	ajaxPromise(url)
+		.then(render)
+		.catch(err => {
+			result.innerHTML = '<h3>Something went wrong</h3>'
+			console.log(err)
+		});
 });
